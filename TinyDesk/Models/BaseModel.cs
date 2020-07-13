7f5e6b144e186ffbce15145cc395a9eea85fe709
Ricardo <ricardovws@gmail.com>
2020-07-13 17:28:40 -0300

    code first

---
+++ TinyDesk/Models/BaseModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace TinyDesk.Models
+{
+    [DataContract]
+    public class BaseModel
+    {
+        [DataMember]
+        public int Id { get; protected set; }
+    }
+
+    public class Product : BaseModel
+    {
+        public Product()
+        {
+
+        }
+
+        [Required]
+        public string Code { get; private set; }
+        [Required]
+        public string Name { get; private set; }
+        [Required]
+        public decimal Price { get; private set; }
+
+        public Product(string code, string name, decimal price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+    }
+
+    public class Register : BaseModel
+    {
+        public Register()
+        {
+        }
+
+        public virtual Order Order { get; set; }
+        [Required]
+        public string Name { get; set; } = "";
+        [Required]
+        public string Email { get; set; } = "";
+        [Required]
+        public string Phone { get; set; } = "";
+        [Required]
+        public string Address { get; set; } = "";
+        [Required]
+        public string Address2 { get; set; } = "";
+        [Required]
+        public string City { get; set; } = "";
+        [Required]
+        public string State { get; set; } = "";
+        [Required]
+        public string Country { get; set; } = "";
+        [Required]
+        public string ZipCode { get; set; } = "";
+    }
+
+    public class ProductOrder : BaseModel
+    {
+        [Required]
+        public Order Order { get; private set; }
+        [Required]
+        public Product Product { get; private set; }
+        [Required]
+        public int Quantity { get; private set; }
+        [Required]
+        public decimal UnitPrice { get; private set; }
+
+        public ProductOrder()
+        {
+        }
+
+        public ProductOrder(Order order, Product product, int quantity, decimal unitPrice)
+        {
+            Order = order;
+            Product = product;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+    }
+
+    public class Order : BaseModel
+    {
+        public Order()
+        {
+            Register = new Register();
+        }
+
+        public Order(Register register)
+        {
+            Register = register;
+        }
+
+        public List<ProductOrder> Itens { get; private set; } = new List<ProductOrder>();
+        [Required]
+        public virtual Register Register { get; private set; }
+    }
+}
